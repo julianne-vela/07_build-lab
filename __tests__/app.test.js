c999ea7ca97aafeb3fa5ef91afbf0f207479ca0f
Julianne Vela <75726146+julianne-vela@users.noreply.github.com>
2021-04-01 16:54:15 -0700

    joke model created, joke-router created

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,13 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+jest.mock('../lib/utils/aws-ses', () => () => ({
+	ResponseMetaData: { RequestId: '11111111-1111' },
+	MessageId: '1111111-1111111-111111-111111',
+}));
+
 describe('joke-api routes', () => {
-  beforeEach(() => {
-    return setup(pool);
-  });
+	beforeEach(() => {
+		return setup(pool);
+	});
 });
--- lib/models/Types.ts
+++
@@ -1,9 +0,0 @@
-export type Joke = {
-  setup: string;
-  punchline: string;
-  joke_type: string;
-};
-
-export type JokeRow = Joke & {
-  _id: string;
-};
