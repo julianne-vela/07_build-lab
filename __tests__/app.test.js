63d61157ffc2d3b652facebc2b2fb47f8ce4fadb
Julianne Vela <75726146+julianne-vela@users.noreply.github.com>
2021-04-01 19:52:38 -0700

    FINALLLLLYYYYY got my getAllJokes test to pass with no sql syntax errors!

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -19,7 +19,46 @@ describe('Joke CRUD Routes', () => {
 	it('returns all jokes in the DB', async () => {
 		const res = await request(app).get('/api/v1/jokes');
 
-		console.log(res.text);
-		expect(res.body).toEqual({ details: 'this and that' });
+		expect(res.body).toEqual({
+			data: [
+				{
+					setup: 'Why did the scarecrow win an award?',
+					punchLine: 'Because he was outstanding in his field.',
+					jokeType: 'general',
+					jokeId: 348,
+					userId: 1,
+				},
+				{
+					setup:
+						'What do you call a fashionable lawn statue with an excellent sense of rhythmn?',
+					punchLine: 'A metro-gnome',
+					jokeType: 'general',
+					jokeId: 216,
+					userId: 1,
+				},
+				{
+					setup: 'How many kids with ADD does it take to change a lightbulb?',
+					punchLine: 'Lets go ride bikes!',
+					jokeType: 'general',
+					jokeId: 142,
+					userId: 2,
+				},
+				{
+					setup: 'A DHCP packet walks into a bar and asks for a beer.',
+					punchLine:
+						'Bartender says, here, but I’ll need that back in an hour!',
+					jokeType: 'programming',
+					jokeId: 384,
+					userId: 2,
+				},
+				{
+					setup: 'What creature is smarter than a talking parrot?',
+					punchLine: 'A spelling bee.',
+					jokeType: 'general',
+					jokeId: 160,
+					userId: 3,
+				},
+			],
+		});
 	});
 });
